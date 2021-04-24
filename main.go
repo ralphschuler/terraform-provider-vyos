72a000d2f7bdc14ce5ef46f7022ed3ab94e5dd4c
Ralph Schuler <ralph.schuler@nyphon.de>
2021-04-24 18:34:28 +0200

    testing !

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-	"github.com/hashicorp/terraform-provider-hashicups/hashicups"
+	"github.com/ralphschuler/terraform-provider-vyos/vyos"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
-			return hashicups.Provider()
+			return vyos.Provider()
 		},
 	})
 }
